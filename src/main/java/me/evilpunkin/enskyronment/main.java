ba1cdcac4c4d6f636bafe8a3dfd41f15f9c64f71
EvilPunkin <82129788+EvilPunkin@users.noreply.github.com>
2021-12-28 13:13:20 -0800

    Initial commit go brrrrrrrrr

---
+++ src/main/java/me/evilpunkin/enskyronment/main.java
@@ -0,0 +1,22 @@
+package me.evilpunkin.enskyronment;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class main {
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
